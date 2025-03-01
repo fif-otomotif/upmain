94cef2360a52228d8c2f98a0d3a7b8712afdfd65
fif-otomotif <fitrakholib@gmail.com>
2025-03-01 18:54:47 +0700

    Update main3.js in bot folder

--- bot/bot.js
+++ bot/bot.js
@@ -1,9 +1,6 @@
 import TelegramBot from "node-telegram-bot-api";
-import { handleJamCommand } from "./jam.js";
 
 const TOKEN = "7827504152:AAG8mfWl81w2n5E7NWCJlaEwLyQrd8KKqfM"; // Ganti dengan token bot-mu
 const bot = new TelegramBot(TOKEN, { polling: true });
-
-handleJamCommand(bot);
 
 export default bot;
